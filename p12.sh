511c22e74b546bfe36a78d4b9ef5b51ef074a183
Pol Graells Pizarro <15583107.clot@fje.edu>
2022-04-08 11:52:24 -0400

    final commit

--- p12.sh
+++ p12.sh
@@ -1,2 +1,44 @@
 #!/bin/bash
 
+if [[ "$(id -u)" != "0" ]]
+then
+	echo "Executa aquet script como root"
+	exit 1
+fi
+resposta="y"
+while [[ $resposta == 'y' ]]
+do
+clear
+
+comprova(){
+DIR="/mnt/cpseg"
+if [ -d "$DIR" ]; then
+   echo "'$DIR' el directori existeix....."
+else
+   echo "Warning: '$DIR' no existeix"
+   mkdir $DIR
+fi
+}
+
+copia(){
+    cp /etc/crontab /mnt/cpseg
+    cp /etc/hosts /mnt/cpseg
+    tar -cvf /mnt/cpseg/backup$(date +"%Y%m%d%H%M").tar.gz /etc/crontab /etc/hosts
+    echo "el fitxer s'ha copiat amb exit!"
+    ls /mnt/cpseg
+    
+}
+
+esborra(){
+    rm -r /mnt/cpseg/crontab
+    rm -r /mnt/cpseg/hosts
+    echo "esborrant fitxers ......."
+    ls /mnt/cpseg
+}
+
+comprova
+copia
+esborra
+
+exit 0
+done
